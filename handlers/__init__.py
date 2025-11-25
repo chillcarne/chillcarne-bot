5d555eafde0dfd3b55d760f1f551607018a552ed
chillcarne <215197792+chillcarne@users.noreply.github.com>
2025-11-25 12:46:41 +0300

    Register Youtube video downloader handlers

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,7 @@
 from telegram.ext import Application
 from .start_handler import start_handler
 from .error_handler import error_handler
+from .youtube_handler import youtube_handler, show_available_formats_callback, download_video_handler_callback, back_to_previous_options_handler_callback
 
 def setup_handlers(app: Application):
     # Start handler
@@ -8,3 +9,10 @@ def setup_handlers(app: Application):
 
     # Error handler
     app.add_error_handler(error_handler)
+
+    # Youtube
+    app.add_handler(youtube_handler)
+    app.add_handler(show_available_formats_callback)
+    app.add_handler(download_video_handler_callback)
+    app.add_handler(back_to_previous_options_handler_callback)
+
